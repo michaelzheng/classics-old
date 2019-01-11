7fefa84f30ec0c307063120dbf865e1f3ff875af
Michael Zheng <michaelzheng54@hotmail.com>
2019-01-11 12:02:29 +0000

    Updates

---
+++ precache-manifest.580641274ecef0acad23cc7845ad5e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d03cdbb1d9cc26be5a96",
+    "url": "/classics/static/css/main.afbcb079.chunk.css"
+  },
+  {
+    "revision": "d03cdbb1d9cc26be5a96",
+    "url": "/classics/static/js/main.d03cdbb1.chunk.js"
+  },
+  {
+    "revision": "cd8544ad8e07391ca368",
+    "url": "/classics/static/js/1.cd8544ad.chunk.js"
+  },
+  {
+    "revision": "d901e1a0fa297ba50b03",
+    "url": "/classics/static/js/runtime~main.d901e1a0.js"
+  },
+  {
+    "revision": "5b5cb9f10f248f9c9287d78327e2a51e",
+    "url": "/classics/static/media/teacher.5b5cb9f1.png"
+  },
+  {
+    "revision": "e592c4f3b4c478eae37b67117df081b5",
+    "url": "/classics/static/media/hermes.e592c4f3.png"
+  },
+  {
+    "revision": "084b45070883f2d32482637fb510f68e",
+    "url": "/classics/index.html"
+  }
+];
