ea4c26643cc64a91a222e0fae150848030340cbe
Michael Zheng <michaelzheng54@hotmail.com>
2019-02-24 22:26:50 +0000

    Updates

---
+++ precache-manifest.e53cae4ceab3e3979a1b0083ddf4e987.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b8843fa282263bea75b",
+    "url": "/classics/static/css/main.0fa8f250.chunk.css"
+  },
+  {
+    "revision": "3b8843fa282263bea75b",
+    "url": "/classics/static/js/main.3b8843fa.chunk.js"
+  },
+  {
+    "revision": "d685fb95435a75d3a019",
+    "url": "/classics/static/js/1.d685fb95.chunk.js"
+  },
+  {
+    "revision": "d901e1a0fa297ba50b03",
+    "url": "/classics/static/js/runtime~main.d901e1a0.js"
+  },
+  {
+    "revision": "1347821e87b32c88d5f8141474f184e6",
+    "url": "/classics/static/media/teacher.1347821e.png"
+  },
+  {
+    "revision": "b06b272662d9d67f0ac41a300315247b",
+    "url": "/classics/static/media/hermes.b06b2726.png"
+  },
+  {
+    "revision": "6ecb86ae6719fd2418de9b6c9a33f883",
+    "url": "/classics/static/media/pigeon.6ecb86ae.png"
+  },
+  {
+    "revision": "d2e44894ddad149734966bdf4a804171",
+    "url": "/classics/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/classics/precache-manifest.e8ff840d2d43fd31c531b85ae7822123.js"
+  "/classics/precache-manifest.e53cae4ceab3e3979a1b0083ddf4e987.js"
 );
 
 workbox.clientsClaim();
